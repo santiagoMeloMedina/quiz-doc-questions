42c2d326530fec81026b483f233e5054e3526a14
Santiago Melo Medina <santiagomelomedina@Santiagos-MacBook-Pro.local>
2022-08-08 12:24:16 -0500

    updated code to save previous attempt

--- quiz_questions.py
+++ quiz_questions.py
@@ -3,6 +3,15 @@ import enum
 import random
 import os
 from typing import Dict, Generator, List, Optional, Tuple
+from itertools import islice
+import signal
+
+
+###########################################################
+### THIS CODE COULD BE GREATLY IMPROVED BY USING CLASSES ##
+###########################################################
+
+PROGRESS_FILE = "progress.db"
 
 
 class ConsoleColors(enum.Enum):
@@ -85,32 +94,97 @@ def get_organized_questions() -> Dict[str, List[Question]]:
     return sections
 
 
-def questions_generator(section: int, randomize: bool) -> Generator[Question, None, None]:
+def questions_generator(
+    section: int, randomize: bool
+) -> Generator[Question, None, None]:
     sections = get_organized_questions()
     if section:
-        keys = {f"{index}":key for index, key in enumerate(sections.keys())}
+        keys = {f"{index}": key for index, key in enumerate(sections.keys())}
         questions = sections[keys[str(section)]]
     else:
         questions = []
         for sec in sections:
             questions += sections[sec]
-            
+
     if randomize:
         random.shuffle(questions)
     yield from questions
 
 
-if __name__ == "__main__":
-    randomize = input("random? y|n: ").lower() == 'y'
-    section = input("section number 0...n: ") if input("section? y|n: ").lower() == 'y' else None
-    questions = questions_generator(section, randomize)
+def get_progress():
+    result = (None, None)
+    try:
+        progress_file = open(PROGRESS_FILE, "r")
+        lines = progress_file.read().split()
+        section = lines[0].split("-")[1]
+        question = lines[1].split("-")[1]
+        result = (section, question)
+    except:
+        print("Could not read progress")
+
+    return result
+
+
+def save_progress(question: int = 0, section: int = None):
+    try:
+        file = open(PROGRESS_FILE, "w")
+        file.write(f"section-{section}\nquestion-{question}")
+    except:
+        print("Could not save progress")
+
+
+def exit_handler(*args, **kwargs):
+    global prog_question, prog_section, randomize
+    if not randomize:
+        save_progress(prog_question, prog_section)
+    exit()
+
+
+def newly_take():
+    global randomize, prog_section
+    randomize = input("random? y|n: ").lower() == "y"
+    prog_section = (
+        input("section number 0...n: ")
+        if input("section? y|n: ").lower() == "y"
+        else None
+    )
+    return questions_generator(prog_section, randomize)
+
+
+def decide_questions():
+    global prog_question, prog_section, randomize
+    prog_question, prog_section, randomize = 0, None, True
+    progress_section, progress_question = get_progress()
+    progress_section, progress_question = eval(progress_section), eval(
+        progress_question
+    )
+    if progress_section or progress_question:
+        if input("reset? y|n: ").lower() == "y":
+            questions = newly_take()
+        else:
+            randomize = False
+            progress_question = progress_question or 0
+            questions = questions_generator(progress_section or 0, False)
+            for _ in range(int(progress_question)):
+                next(questions)
+            prog_question, prog_section = progress_question, progress_section
+    else:
+        questions = newly_take()
+
+    return questions
+
+
+def start_quiz(questions: Generator[Question, None, None]):
+    global prog_question
+    input("Try to answer questions in depth OK?")
     right, wrong = 0, 0
     stop = False
-    input("Try to answer questions in depth OK?")
     while not stop:
         question = next(questions, None)
+        prog_question += 1
         if question is None:
             stop = True
+            save_progress()
             os.system("clear")
             print("Finished!")
             print(f"Results:\n    Correct: {right}\n    Incorrect: {wrong}")
@@ -135,3 +209,8 @@ if __name__ == "__main__":
                 right += 1
             else:
                 wrong += 1
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, exit_handler)
+    start_quiz(decide_questions())
